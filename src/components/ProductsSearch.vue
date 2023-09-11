81eac4c925beef76699d794655b98e54404908b4
Luiz Otávio <luizotavio0106@outlook.com>
2023-09-11 20:17:39 -0300

    feat: creating home

---
+++ src/components/ProductsSearch.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Products Search</div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Home</div>
+  <section>
+    <h1>👍️ Compre ou Venda 👎️</h1>
+    <ProductsSearch />
+    <ProductsList />
+  </section>
 </template>
 
 <script>
+import ProductsSearch from '@/components/ProductsSearch.vue';
+import ProductsList from '@/components/ProductsList.vue';
+
 export default {
   name: 'HomeView',
-  components: {},
+  components: {
+    ProductsSearch,
+    ProductsList,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
