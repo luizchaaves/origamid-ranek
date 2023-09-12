e97b2e4a12a0704b897591810b3c478030245630
Luiz Otávio <luizotavio0106@outlook.com>
2023-09-11 22:48:29 -0300

    style: change the ProductList and ProductSearch components

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,13 +1,18 @@
 <template>
   <form>
-    <input type="text" name="search" id="search" v-model="search" />
+    <input
+      type="text"
+      name="search"
+      id="search"
+      v-model="search"
+      placeholder="Buscar..."
+    />
     <input
       type="submit"
       id="searchButton"
       value="Buscar"
       @click.prevent="searchProducts"
     />
-    {{ search }}
   </form>
 </template>
 
@@ -33,3 +38,35 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#search {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#search:focus,
+#search:hover {
+  transform: scale(1.1);
+}
+
+#searchButton {
+  width: 62px;
+  height: 62px;
+  background: url('../assets/search.svg') no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
+</style>
