7a0a9f842d7337c4315832f651e6379b579dd5eb
Luiz Otávio <luizotavio0106@outlook.com>
2023-09-12 20:46:44 -0300

    feat: update user data

--- src/views/user/UserEdit.vue
+++ src/views/user/UserEdit.vue
@@ -1,9 +1,40 @@
 <template>
-  <p>UserEdit</p>
+  <UserForm>
+    <button class="btn" @click.prevent="atualizarUsuario">
+      Atualizar Usuário
+    </button>
+  </UserForm>
 </template>
 
 <script>
+import UserForm from '@/components/UserForm.vue';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+import { api } from '@/services';
+
 export default {
   name: 'UserEdit',
+  components: { UserForm },
+
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+
+    const atualizarUsuario = () => {
+      api
+        .put(`/usuario/${store.state.usuario.id}`, store.state.usuario)
+        .then(() => {
+          store.dispatch('getUsuario', store.state.usuario.id);
+          router.push({ name: 'user' });
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    };
+
+    return {
+      atualizarUsuario,
+    };
+  },
 };
 </script>
