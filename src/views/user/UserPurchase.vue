72cf1cc8d7b2cddae8d08d910139256e6e128ffe
Luiz Otávio <luizotavio0106@outlook.com>
2023-09-14 16:30:19 -0300

    feat: show user purchases and sales

--- src/views/user/UserPurchase.vue
+++ src/views/user/UserPurchase.vue
@@ -1,3 +1,68 @@
 <template>
-  <p>UserPurchase</p>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div
+        class="produto-wrapper"
+        v-for="(compra, index) in compras"
+        :key="index"
+      >
+        <ProductItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor">
+            <span>Vendedor: </span>{{ compra.vendedor_id }}
+          </p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue';
+import { ref, computed, watch } from 'vue';
+import { useStore } from 'vuex';
+import { api } from '@/services';
+
+export default {
+  name: 'UserPurchase',
+  components: {
+    ProductItem,
+  },
+
+  setup() {
+    const store = useStore();
+
+    const compras = ref(null);
+    const usuario = computed(() => store.state.usuario);
+    const login = computed(() => store.state.login);
+
+    const getCompras = () => {
+      api
+        .get(`/transacao?comprador_id=${usuario.value.id}`)
+        .then((response) => (compras.value = response.data));
+    };
+
+    if (login.value) getCompras();
+
+    watch(login, () => getCompras());
+
+    return {
+      compras,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+</style>
--- src/views/user/UserSales.vue
+++ src/views/user/UserSales.vue
@@ -1,9 +1,88 @@
 <template>
-  <p>UserSales</p>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div
+        class="produto-wrapper"
+        v-for="(venda, index) in vendas"
+        :key="index"
+      >
+        <ProductItem v-if="venda.produto" :produto="venda.produto">
+          <p class="comprador">
+            <span>Comprador: </span>{{ venda.comprador_id }}
+          </p>
+        </ProductItem>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value, key) in venda.endereco" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductItem from '@/components/ProductItem.vue';
+import { ref, computed, watch } from 'vue';
+import { useStore } from 'vuex';
+import { api } from '@/services';
+
 export default {
   name: 'UserSales',
+  components: {
+    ProductItem,
+  },
+
+  setup() {
+    const store = useStore();
+
+    const vendas = ref(null);
+    const usuario = computed(() => store.state.usuario);
+    const login = computed(() => store.state.login);
+
+    const getVendas = () => {
+      api
+        .get(`/transacao?vendedor_id=${usuario.value.id}`)
+        .then((response) => (vendas.value = response.data));
+    };
+
+    if (login.value) getVendas();
+
+    watch(login, () => getVendas());
+
+    return {
+      vendas,
+    };
+  },
 };
 </script>
+
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.comprador span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+}
+
+h3 {
+  margin: 0;
+  justify-self: end;
+}
+</style>
